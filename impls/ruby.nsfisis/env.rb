fd88367ac0febbf553bb791a319b2b818260bea4
nsfisis <nsfisis@gmail.com>
2024-04-01 23:22:45 +0900

    step3

---
+++ impls/ruby.nsfisis/env.rb
@@ -0,0 +1,33 @@
+class Env
+  def initialize(outer)
+    @outer = outer
+    @data = {}
+  end
+
+  def set(k, v)
+    @data[k] = v
+  end
+
+  def find(k)
+    if @data.has_key?(k)
+      self
+    elsif @outer
+      @outer.find(k)
+    else
+      nil
+    end
+  end
+
+  def get(k)
+    e = find(k)
+    if e
+      e.get_local(k)
+    else
+      raise "'#{k}' not found"
+    end
+  end
+
+  def get_local(k)
+    @data[k]
+  end
+end
---
+++ impls/ruby.nsfisis/step3_env.rb
@@ -0,0 +1,92 @@
+require_relative './reader.rb'
+require_relative './printer.rb'
+require_relative './env.rb'
+
+def read_
+  s = gets
+  return unless s
+  read_str(s.chomp)
+end
+
+def apply(value, env)
+  case value.first
+  when :'def!'
+    raise "invalid 'def!' form" unless value.length == 3
+    k = value[1]
+    v = eval_(value[2], env)
+    env.set(k, v)
+  when :'let*'
+    raise "invalid 'let*' form" unless value.length == 3
+    bindings = value[1]
+    raise "'let*' binding(s) must be a list" unless bindings.is_a?(Array)
+    raise "'let*' binding(s) must have odd elements" unless bindings.length % 2 == 0
+    expr = value[2]
+    new_env = Env.new(env)
+    bindings.each_slice(2) do |kv|
+      k = kv[0]
+      v = eval_(kv[1], new_env)
+      new_env.set(k, v)
+    end
+    eval_(expr, new_env)
+  else
+    value = eval_ast(value, env)
+    x = value.first
+    xs = value[1..]
+    x[*xs]
+  end
+end
+
+def eval_(value, env)
+  case value
+  when Array
+    if value.empty?
+      value
+    else
+      apply(value, env)
+    end
+  else
+    eval_ast(value, env)
+  end
+end
+
+def eval_ast(ast, env)
+  case ast
+  when Symbol
+    env.get(ast)
+  when Array
+    ast.map { eval_(_1, env) }
+  else
+    ast
+  end
+end
+
+def print_(value)
+  s = pr_str(value)
+  puts s
+end
+
+def rep(env)
+  print "user> "
+  input = read_
+  return false unless input
+  result = eval_ input, env
+  print_ result
+  return true
+rescue RuntimeError => e
+  STDERR.puts "ERROR:"
+  STDERR.puts e
+  return true
+end
+
+def repl
+  env = Env.new(nil)
+  env.set(:'+', ->(a, b) { a + b })
+  env.set(:'-', ->(a, b) { a - b })
+  env.set(:'*', ->(a, b) { a * b })
+  env.set(:'/', ->(a, b) { a / b })
+
+  while rep(env)
+  end
+end
+
+repl
